15fdda161b17fc296829d9427d2331d0a76bc229
Anton <antonolsson2005@gmail.com>
2023-09-18 13:47:55 +0200

    yo

---
+++ venv/views.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, redirect, url_for
+from flask import render_template
+
+views = Blueprint(__name__, "")
+
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+@views.route("/produkter")
+def produkter():
+    return render_template("produkter.html")
+
+@views.route("/om_oss")
+def om_oss():
+    return render_template("om oss.html")
+
+@views.route("/nyheter")
+def nyheter():
+    return render_template("nyheter.html")
+
